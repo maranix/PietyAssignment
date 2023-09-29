3397b9bc269018ee8a55477d554ac74ab981a2e0
maranix <ramanverma4183@gmail.com>
2023-09-29 21:42:49 +0530

    chore(bloc_observer): add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:piety_assignment/src/app/observer.dart';
 import 'package:piety_assignment/src/app/view/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: Add firebase config
+  // await Firebase.initializeApp();
+
+  Bloc.observer = PietyAppBlocObserver();
+
   runApp(const PietyApp());
 }
